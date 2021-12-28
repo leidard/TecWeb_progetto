28c0cf8ef0f046c35ad6c25d577ea2cc2416584b
BackToFrancesco <bacchin.francesco2000@gmail.com>
2021-12-28 16:51:30 +0000

    Modifica index

--- controllers/index.php
+++ controllers/index.php
@@ -12,11 +12,12 @@ $main = file_get_contents('../views/index.html');
 /**
  * Vari str_replace nella vista main
  */
-$main = str_replace('%TITOLO%', "123", $main);
-
+$header = str_replace('%TITOLO%', 'Scissorhands', $header);
 
 $pagina = str_replace('%HEADER%', $header, $pagina);
 $pagina = str_replace('%FOOTER%', $footer, $pagina);
 $pagina = str_replace('%MAIN%', $main, $pagina);
+
+
 
 echo $pagina;
