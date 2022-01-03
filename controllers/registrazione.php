09c39a15bfe3ea654ed927229b7b4825726620ee
matt8227 <matteo.bellato20@gmail.com>
2022-01-03 23:31:13 +0100

    Aggiunta funzione (rotta) di registrazione
    Rotta per via del DB che ha problemi con le fk di credential

--- controllers/registrazione.php
+++ controllers/registrazione.php
@@ -2,17 +2,34 @@
 require_once 'components/page.php';
 require_once 'components/header.php';
 
+require_once __DIR__ . '/../services/public/registration.php';
+
 $pagina = page('Registrazione - Scissorhands');
+
 $path = array(
     "Home" => "/",
-    "Registrazione" => "/registrazione.php"
+    "Registrazione" => "registrazione.php"
 );
 
-$header = _header('Registrazione', $path);
-
+$header = _header('Registrazione',$path);
+$footer = _footer();
 $main = file_get_contents('../views/user/registrazione.html');
 
-#CF, cognome, nome, data nascita
+
+
+
+if(isset($_GET["name"]) && isset($_GET["surname"]) && isset($_GET["sex"]) && isset($_GET["mail"]) && isset($_GET["password"]))
+{
+	if(filter_var($_GET["mail"], FILTER_VALIDATE_EMAIL)) # Da considerare i falsi positivi/negativi
+	{
+		RegistrationService::RegisterUser($_GET["name"], $_GET["surname"], $_GET["sex"], $_GET["mail"], $_GET["password"]);
+	}
+	else
+	{
+		$main.="Email non valida."; # Da riempire meglio ofc.
+	}
+}
+
 
 $pagina = str_replace('%HEADER%', $header, $pagina);
 $pagina = str_replace('%MAIN%', $main, $pagina);
--- services/public/registration.php
+++ services/public/registration.php
@@ -1,15 +1,16 @@
 <?php
 
-require_once __DIR__.'../../models/credential.php';
-require_once __DIR__.'../../models/customer.php';
+#require_once __DIR__ . '../models/credential.php';
+require_once __DIR__ . '/../../models/credential.php';
+require_once __DIR__ . '/../../models/customer.php';
 
 class RegistrationService
 {
-	public static function RegisterUser($cf, $name, $surname, $date_of_birth, $sex, $email, $password)
+	public static function RegisterUser($name, $surname, $sex, $email, $password)
 	{
 		$ex = new Customer();
-		$ex->create($cf, $name, $surname, $date_of_birth, $sex);
+		$lastid =$ex->create($name, $surname,  $sex)[0]; #TESTARE
 		$ez = new Credential();
-		$ez->create($cf, $email, $password, $type);
+		$ez->create($email, $password, "USER", $lastid); #For LAST_INSERT_ID(), the most recently generated ID is maintained in the server on a per-connection basis
 	}
 }
