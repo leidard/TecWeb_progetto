d7ddf1a4299b147f848225e7178d7d4486e33dd0
Leila <leila.dardouri@outlook.it>
2022-01-30 18:23:09 +0000

    added breakpoint

--- controllers/registrati.php
+++ controllers/registrati.php
@@ -93,7 +93,7 @@ if(isset($_POST["submit"]) && isset($name) && isset($surname) && isset($mail) &&
 		$ex = RegistrationService::RegisterUser($name, $surname, strtolower($mail), $password);
 		if($ex)
 		{
-			$_SESSION["regcomplete"] = "Registrazione completata! <a href=\"/accedi.php\">clicca qui per effettuare l'accesso<a>.";
+			$_SESSION["regcomplete"] = "Registrazione completata! <a href=\"/accedi.php\">Clicca qui per effettuare l'accesso<a>.";
 			#unset everything??????????????
 		}
 		else
