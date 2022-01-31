38379b07dc3724a4c4444be41e4dc2b32a6f45b8
matt8227 <matteo.bellato20@gmail.com>
2022-01-31 21:46:48 +0100

    Separato js + controlli ez

---
+++ controllers/staff/orari.js
@@ -0,0 +1,56 @@
+(function() {
+	let openEl = document.getElementById('open_at');
+	let closeEl = document.getElementById('close_at');
+	let error = document.getElementById('orario-error');
+	let change = document.getElementById('change');
+
+	function refresh() {
+		closeEl.setAttribute('min', openEl.valueAsNumber+1 || 0);
+		openEl.setAttribute('max', closeEl.valueAsNumber-1 || 23);
+		if(closeEl.valueAsNumber === openEl.valueAsNumber)
+		{
+			//error.innerText="Gli orari non possono essere uguali.";
+		}
+		else
+		{
+			//error.innerText="";
+		}
+	}
+	refresh();
+
+	function check(event)
+	{
+		error.innerText="";
+		error.removeAttribute("aria-live");
+		if(closeEl.valueAsNumber<=openEl.valueAsNumber)
+		{
+			event.preventDefault();
+			error.innerText="L'apertura deve precedere la chiusura.";
+			error.setAttribute("aria-live","polite");
+			refresh();
+		}
+		if(closeEl.valueAsNumber > 23)
+		{
+			event.preventDefault();
+			error.innerText="Orario chiusura non valido.";
+			error.setAttribute("aria-live","polite");
+		}
+		if(openEl.valueAsNumber < 0)
+		{
+			event.preventDefault();
+			error.innerText="Orario apertura non valido.";
+			error.setAttribute("aria-live","polite");
+
+		}
+	}
+
+	change.onclick = check;
+	///openEl.onkeydown = check;
+	//closeEl.onkeydown = check;
+
+	openEl.oninput = refresh;
+	closeEl.oninput = refresh;
+
+
+
+})();
