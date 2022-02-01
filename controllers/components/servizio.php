257770deb90ca0eacecef385880b0b283ce7e6bc
Leila <leila.dardouri@outlook.it>
2022-02-01 18:56:59 +0000

    removed (?)

--- controllers/components/servizio.php
+++ controllers/components/servizio.php
@@ -4,7 +4,7 @@ function _servizio($nome, $prezzo, $durata = '', $descrizione = '') {
     $out = file_get_contents(__DIR__.'/../../views/components/servizio.html');
     $out = str_replace('%NOME%', $nome, $out);
     $out = str_replace('%PREZZO%', number_format($prezzo, 2, ",", " "), $out);
-    $out = str_replace('%DURATA%', $durata/60, $out); //(?)
+    $out = str_replace('%DURATA%', $durata/60, $out);
     $out = str_replace('%DESCRIZIONE%', $descrizione, $out);
 
     return $out;
